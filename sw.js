581a6dd1e5c0ceb62cbd93fb813cfe14ea9e1494
LumiShop <lumishopoficial@gmail.com>
2026-08-27 16:44:13 -0600

    Commit inicial de la agenda

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'ventas-mejenku-v1';
+
+const ASSETS = [
+  './Agenda/index.html',
+  './Agenda/Agenda.html',
+  './Agenda/Dinero.html',
+  './img/Logo.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
